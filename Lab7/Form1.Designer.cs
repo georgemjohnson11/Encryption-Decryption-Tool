8e28c6d5de4c864f7b8db9521460f19f8a2f242b
georgemjohnson11 <georgemjohnson11@gmail.com>
2016-06-06 17:44:43 -0400

    Upload of Code
    Final Version.

---
+++ Lab7/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Filenametext = new System.Windows.Forms.TextBox();
+            this.Keytext = new System.Windows.Forms.TextBox();
+            this.Filenamelabel = new System.Windows.Forms.Label();
+            this.Keylabel = new System.Windows.Forms.Label();
+            this.encryptbutton = new System.Windows.Forms.Button();
+            this.decryptbutton = new System.Windows.Forms.Button();
+            this.browsecompfiles = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // Filenametext
+            // 
+            this.Filenametext.Location = new System.Drawing.Point(22, 61);
+            this.Filenametext.Name = "Filenametext";
+            this.Filenametext.Size = new System.Drawing.Size(265, 20);
+            this.Filenametext.TabIndex = 0;
+            // 
+            // Keytext
+            // 
+            this.Keytext.Location = new System.Drawing.Point(22, 110);
+            this.Keytext.Name = "Keytext";
+            this.Keytext.Size = new System.Drawing.Size(112, 20);
+            this.Keytext.TabIndex = 1;
+            // 
+            // Filenamelabel
+            // 
+            this.Filenamelabel.AutoSize = true;
+            this.Filenamelabel.Location = new System.Drawing.Point(31, 45);
+            this.Filenamelabel.Name = "Filenamelabel";
+            this.Filenamelabel.Size = new System.Drawing.Size(52, 13);
+            this.Filenamelabel.TabIndex = 2;
+            this.Filenamelabel.Text = "Filename:";
+            // 
+            // Keylabel
+            // 
+            this.Keylabel.AutoSize = true;
+            this.Keylabel.Location = new System.Drawing.Point(31, 94);
+            this.Keylabel.Name = "Keylabel";
+            this.Keylabel.Size = new System.Drawing.Size(28, 13);
+            this.Keylabel.TabIndex = 3;
+            this.Keylabel.Text = "Key:";
+            // 
+            // encryptbutton
+            // 
+            this.encryptbutton.Location = new System.Drawing.Point(34, 148);
+            this.encryptbutton.Name = "encryptbutton";
+            this.encryptbutton.Size = new System.Drawing.Size(75, 23);
+            this.encryptbutton.TabIndex = 4;
+            this.encryptbutton.Text = "Encrypt";
+            this.encryptbutton.UseVisualStyleBackColor = true;
+            this.encryptbutton.Click += new System.EventHandler(this.encryptbutton_Click);
+            // 
+            // decryptbutton
+            // 
+            this.decryptbutton.Location = new System.Drawing.Point(136, 148);
+            this.decryptbutton.Name = "decryptbutton";
+            this.decryptbutton.Size = new System.Drawing.Size(75, 23);
+            this.decryptbutton.TabIndex = 5;
+            this.decryptbutton.Text = "Decrypt";
+            this.decryptbutton.UseVisualStyleBackColor = true;
+            this.decryptbutton.Click += new System.EventHandler(this.decryptbutton_Click);
+            // 
+            // browsecompfiles
+            // 
+            this.browsecompfiles.Image = global::Lab7.Properties.Resources.OpenPH1;
+            this.browsecompfiles.Location = new System.Drawing.Point(293, 45);
+            this.browsecompfiles.Name = "browsecompfiles";
+            this.browsecompfiles.Size = new System.Drawing.Size(39, 36);
+            this.browsecompfiles.TabIndex = 6;
+            this.browsecompfiles.UseVisualStyleBackColor = true;
+            this.browsecompfiles.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 193);
+            this.Controls.Add(this.browsecompfiles);
+            this.Controls.Add(this.decryptbutton);
+            this.Controls.Add(this.encryptbutton);
+            this.Controls.Add(this.Keylabel);
+            this.Controls.Add(this.Filenamelabel);
+            this.Controls.Add(this.Keytext);
+            this.Controls.Add(this.Filenametext);
+            this.Name = "Form1";
+            this.Text = "Encrypter/Decrypter by George Johnson";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Filenametext;
+        private System.Windows.Forms.TextBox Keytext;
+        private System.Windows.Forms.Label Filenamelabel;
+        private System.Windows.Forms.Label Keylabel;
+        private System.Windows.Forms.Button encryptbutton;
+        private System.Windows.Forms.Button decryptbutton;
+        private System.Windows.Forms.Button browsecompfiles;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+    }
+}
+
---
+++ Lab7/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Lab7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //lets the user choose the file
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                Filenametext.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        //takes the input file and checks to ensure there is a file and the key to encrypt to make a .des file
+        private void encryptbutton_Click(object sender, EventArgs e)
+        {
+            FileStream infile = null, outfile = null;
+            byte[] buffer = new byte[512];
+            byte[] key = new byte[8];
+            if (Filenametext.Text == "")
+            {
+                MessageBox.Show("The source file must be specified!");
+                return;
+
+            }
+                //ensures that a key is used
+            else if (Keytext.Text == "")
+            {
+                MessageBox.Show("The Key must be specified!");
+                return;
+            }
+                //ensures a good file type is used.
+            else if (Filenametext.Text.Substring(Filenametext.TextLength - 4, 4) == ".des")
+            {
+                MessageBox.Show("Encrypting more than once leads to file errors. Please use another file not encrypted");
+                return;
+            }
+            try
+            {
+                infile = new FileStream(Filenametext.Text, FileMode.Open);
+                if (File.Exists(Filenametext.Text+".des"))
+                    { 
+                    // allows the user to overwrite a file
+                        DialogResult result = MessageBox.Show("Do you want to OVERWRITE existing file?", "Warning",
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                        if (result == DialogResult.Yes)
+                        {
+                            File.Delete(Filenametext.Text+".des");
+                        }
+                        else if (result == DialogResult.No)
+                        {
+                            return;
+                        }
+                    }
+                outfile = new FileStream(Filenametext.Text + ".des", FileMode.Create);
+                byte[] bin = new byte[100]; //This is intermediate storage for the encryption.
+                long rdlen = 0;              //This is the total number of bytes written.
+                long totlen = infile.Length;    //This is the total length of the input file.
+                int len;                     //This is the number of bytes to be written at a time.
+                DES des = new DESCryptoServiceProvider();
+                byte[] encryptkey = new byte[8];
+                int i = 0;
+
+                //assigns the lower bytes to the proper location for keys that are not 8 char in length
+                foreach (char k in Keytext.Text)
+                {
+                    byte keys = (byte)k;
+                    encryptkey[i] += keys;
+                    ++i;
+                    if (i == 8)
+                    { i = 0; }
+                }
+                des.Key = encryptkey;
+                des.IV = encryptkey;
+                CryptoStream crypt = new CryptoStream(outfile, des.CreateEncryptor(), CryptoStreamMode.Write);
+
+                while (rdlen < totlen)
+                {
+                    len = infile.Read(bin, 0, 100);
+                    crypt.Write(bin, 0, len);
+                    rdlen = rdlen + len;
+                    Console.WriteLine("{0} bytes processed", rdlen);
+                }
+
+                crypt.Close();
+                outfile.Close();
+                infile.Close();
+            }
+                //checks for good or bad key
+                catch (System.Security.Cryptography.CryptographicException)
+            {
+                MessageBox.Show("Corrupt or bad Key", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+                //checks for good or bad file
+                catch (System.IO.FileNotFoundException)
+            {
+                MessageBox.Show("Could not open file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show("Encryption failed!\n" + ee.Message);
+            }
+        }
+
+
+        private void decryptbutton_Click(object sender, EventArgs e)
+        {
+            FileStream infile = null, outfile = null;
+            byte[] buffer = new byte[512];
+            byte[] key = new byte[8];
+            //ensures user has a file to en/decrypt
+            if (Filenametext.Text == "")
+            {
+                MessageBox.Show("The source file must be specified!");
+                return;
+
+            }
+                //ensures user has a key they are using
+            else if (Keytext.Text == "")
+            {
+                MessageBox.Show("The Key must be specified!");
+                return;
+            }
+                //makes sure it's the correct file extension
+            else if (Filenametext.Text.Substring(Filenametext.TextLength - 4, 4) != ".des")
+            {
+                MessageBox.Show("Only .des file types are allowed");
+                return;
+        }
+            else if (Filenametext.Text.Substring(Filenametext.TextLength - 4, 4) == ".des")
+            {
+                try
+                {
+                    infile = new FileStream(Filenametext.Text, FileMode.Open);
+                    if (File.Exists(Filenametext.Text.Substring(0,Filenametext.TextLength -4)))
+                    {
+                        //ensures the user does not unintentionally overwrite file to prevent loss of key
+                        DialogResult result = MessageBox.Show("Do you want to OVERWRITE existing file?", "Warning",
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                        if (result == DialogResult.Yes)
+                        {
+                            File.Delete(Filenametext.Text.Substring(0, Filenametext.TextLength - 4));
+                        }
+                        else if (result == DialogResult.No)
+                        {
+                            return;
+                        }
+                         }
+                    outfile = new FileStream(Filenametext.Text.Remove(Filenametext.TextLength - 4, 4), FileMode.Create);
+                    byte[] bin = new byte[100]; //This is intermediate storage for the encryption.
+                    long rdlen = 0;              //This is the total number of bytes written.
+                    long totlen = infile.Length;    //This is the total length of the input file.
+                    int len;                     //This is the number of bytes to be written at a time.
+                    DES des = new DESCryptoServiceProvider();
+                    //converts keys to byte based 
+                    byte[] decryptkey = new byte[8];
+                    int i=0;
+                    foreach (char k in Keytext.Text)
+                    {
+                        byte keys = (byte)k;
+                        decryptkey[i]+= keys;
+                        ++i;
+                            if (i==8)
+                            {i=0;}
+                    }
+                    des.Key = decryptkey;
+                    des.IV = decryptkey;
+                    CryptoStream crypt = new CryptoStream(outfile, des.CreateDecryptor(), CryptoStreamMode.Write);
+                    while (rdlen < totlen)
+                    {
+                        len = infile.Read(bin, 0, 100);
+                        crypt.Write(bin, 0, len);
+                        rdlen = rdlen + len;
+                        Console.WriteLine("{0} bytes processed", rdlen);
+                    }
+
+                    crypt.Close();
+                    outfile.Close();
+                    infile.Close();
+                }
+                //checks for good or bad key
+                catch (System.Security.Cryptography.CryptographicException)
+                {
+                    MessageBox.Show("Corrupt or bad Key", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                //checks for good or bad file
+                catch (System.IO.FileNotFoundException)
+                {
+                    MessageBox.Show("Could not open file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                //general log of failed decryption
+                catch (Exception ee)
+                {
+                    MessageBox.Show("Decryption failed!\n" + ee.Message);
+                }
+
+            }
+        }
+    }
+}
+    
